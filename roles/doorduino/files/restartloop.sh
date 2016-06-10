7a75fda67f77b8a8fd6fdded4b3148588f684aea
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2016-06-11 01:51:03 +0200

    Doorduino files, pass lookup plugin, misc files

---
+++ roles/doorduino/files/restartloop.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+trap 'echo "--------------"' 2
+
+if [ "$1" == "" ]; then
+    echo Usage: $0 /dev/ttyUSBx
+    exit 255
+fi
+
+while true; do
+    sudo chmod 666 $1
+    perl doorduino.pl $1
+done
+
